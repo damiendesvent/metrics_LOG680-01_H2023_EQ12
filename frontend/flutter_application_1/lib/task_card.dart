d22d10c740cdb2f44be64277e18846daf2ebfa60
Bruno Moya <elblogdebruno@outlook.es>
2023-02-03 18:04:32 -0500

    feat: refactoring to classes, fixed column height

---
+++ frontend/flutter_application_1/lib/task_card.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class TaskCard extends StatelessWidget {
+  TaskCard(this.task, {super.key});
+
+  Map task;
+
+  var chipNames = [];
+  String taskName = "";
+  String status = "";
+  String assigneeName = "";
+  String creatorName = "";
+  String createdAt = "";
+
+  @override
+  Widget build(BuildContext context) {
+    taskName = task['content']['title'];
+    status = task['fieldValues']['nodes'][0]['name'];
+    assigneeName = task['content']['assignees']['nodes'].isEmpty
+        ? ""
+        : task['content']['assignees']['nodes'][0]['login'];
+    if (assigneeName.isNotEmpty && !chipNames.contains(assigneeName)) {
+      chipNames.add(assigneeName);
+    }
+    creatorName = task['creator']['login'];
+    createdAt = task['fieldValues']['nodes'][0]['createdAt'].substring(
+        0, task['fieldValues']['nodes'][0]['createdAt'].indexOf('T'));
+    return Container(
+        padding: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(
+                color: const Color.fromRGBO(191, 191, 191, 1), width: 1)),
+        child: Table(columnWidths: const {
+          1: FractionColumnWidth(0.25)
+        }, children: [
+          TableRow(children: [
+            Text(
+              taskName,
+              style: const TextStyle(fontWeight: FontWeight.w900),
+            ),
+            Container(alignment: Alignment.centerRight, child: Text(status))
+          ]),
+          TableRow(children: [
+            Container(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 15),
+                child: Text(
+                  "Créé par $creatorName le $createdAt",
+                  style: const TextStyle(fontSize: 12, color: Colors.blueGrey),
+                )),
+            Container()
+          ]),
+          TableRow(children: [
+            Wrap(
+              spacing: 5,
+              children: List<Widget>.generate(chipNames.length, (int index) {
+                return Chip(
+                    label: Text(chipNames[index],
+                        style: const TextStyle(
+                            fontSize: 12, fontWeight: FontWeight.w600)),
+                    backgroundColor: Colors.amber);
+              }),
+            ),
+            Container(
+                alignment: Alignment.centerRight,
+                child: Tooltip(
+                    message: "Temps de complétion",
+                    child: Chip(
+                        backgroundColor: Colors.transparent,
+                        avatar: const Icon(Icons.access_time_rounded),
+                        shape: RoundedRectangleBorder(
+                            borderRadius:
+                            const BorderRadius.all(Radius.circular(50)),
+                            side: BorderSide(
+                                color: Colors.grey.shade600, width: 1)),
+                        label: const Text(
+                          "5d:5h",
+                          style: TextStyle(fontWeight: FontWeight.w600),
+                        ))))
+          ])
+        ]));
+  }
+}
---
+++ frontend/flutter_application_1/lib/task_list.dart
@@ -0,0 +1,194 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/task_card.dart';
+
+import 'main.dart';
+
+class TaskList extends StatefulWidget {
+  const TaskList({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  DateTimeRange? _selectedDateRange;
+
+  List selectedTasks = [];
+  var dropdownvalue = columns.first['name'];
+  var chipNames = ["Semaine", "Mois", "Max"];
+  var dateString = "sur toute la période";
+  int? selectedIndex = 2;
+  List<Widget> cards = [];
+  bool init = true;
+
+  @override
+  void initState() {
+    super.initState();
+    updateSelectTasks(dropdownvalue);
+  }
+
+  void _show() async {
+    final DateTimeRange? result = await showDateRangePicker(
+        context: context,
+        firstDate: DateTime(2020, 1, 1),
+        lastDate: DateTime(2030, 12, 31),
+        initialDateRange:
+        DateTimeRange(start: DateTime(2020, 1, 1), end: DateTime.now()),
+        currentDate: DateTime.now(),
+        saveText: 'Done',
+        initialEntryMode: DatePickerEntryMode.inputOnly);
+
+    if (result != null) {
+      // Rebuild the UI
+      setState(() {
+        _selectedDateRange = result;
+      });
+    }
+    _dateString(_selectedDateRange?.start, _selectedDateRange?.end, "Specific");
+    selectedIndex = null;
+  }
+
+  void _dateString(DateTime? startDt, DateTime? endDt, String? mode) {
+    switch (mode) {
+      case "Max":
+        {
+          dateString = "sur toute la période";
+        }
+        break;
+      case "Mois":
+        {
+          dateString =
+          "du ${DateTime.now().subtract(const Duration(days: 31)).toString().split(' ')[0]} au ${DateTime.now().toString().split(' ')[0]}";
+        }
+        break;
+      case "Semaine":
+        {
+          dateString =
+          "du ${DateTime.now().subtract(const Duration(days: 7)).toString().split(' ')[0]} au ${DateTime.now().toString().split(' ')[0]}";
+        }
+        break;
+      case "Specific":
+        {
+          String startDate = startDt.toString().split(' ')[0];
+          String endDate = endDt.toString().split(' ')[0];
+          dateString = "du $startDate au $endDate";
+        }
+        break;
+    }
+  }
+
+  final ButtonStyle style = ElevatedButton.styleFrom(
+    textStyle: const TextStyle(fontSize: 20),
+  );
+
+  void updateSelectTasks(var columnName) {
+    setState(() {
+      selectedTasks.clear();
+      for (var task in tasks) {
+        if (task['fieldValues']['nodes'][0]['name'] == columnName) {
+          selectedTasks.add(task);
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Card(
+            child: Container(
+              width: double.maxFinite,
+              padding: const EdgeInsets.all(15.0),
+              child: Wrap(
+                spacing: 15,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                direction: Axis.horizontal,
+                children: [
+                  const Icon(Icons.view_list_rounded, size: 25),
+                  const Text(" Tâche de "),
+                  SizedBox(
+                    width: 150.0,
+                    child: DropdownButtonFormField(
+                      value: dropdownvalue,
+                      items: columns.map((Map item) {
+                        return DropdownMenuItem(
+                          value: item['name'],
+                          child: Text(item['name']),
+                        );
+                      }).toList(),
+                      onChanged: (var newValue) {
+                        setState(() {
+                          dropdownvalue = newValue;
+                          updateSelectTasks(dropdownvalue);
+                        });
+                      },
+                    ),
+                  ),
+                  Text(dateString),
+                  ElevatedButton(
+                    style: style,
+                    onPressed: _show,
+                    child:
+                    const Icon(Icons.date_range_rounded, color: Colors.white),
+                  )
+                ],
+              ),
+            )),
+        const SizedBox(height: 3),
+        Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Expanded(
+              child: Card(
+                  child: Container(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Wrap(
+                        spacing: 15,
+                        children: List<Widget>.generate(chipNames.length,
+                                (int index) {
+                              return InputChip(
+                                  label: Text(chipNames[index]),
+                                  selected: selectedIndex == index,
+                                  onSelected: (bool selected) {
+                                    setState(() {
+                                      if (selectedIndex != index) {
+                                        _dateString(
+                                            _selectedDateRange?.start,
+                                            _selectedDateRange?.end,
+                                            chipNames[index]);
+                                        selectedIndex = index;
+                                      }
+                                    });
+                                  });
+                            }),
+                      )))),
+          Card(
+              child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Tooltip(
+                      message: "Lead time moyen",
+                      child: Chip(
+                          avatar: const Icon(Icons.access_time_rounded),
+                          backgroundColor: Colors.blue.shade100,
+                          label: const Text(
+                            "5d:5h",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          )))))
+        ]),
+        const SizedBox(height: 3),
+        Expanded(
+            child: Card(
+                child: Container(
+                    width: double.maxFinite,
+                    padding: const EdgeInsets.all(15.0),
+                    child: ListView.separated(
+                        itemBuilder: (task, index) =>
+                            TaskCard(selectedTasks[index]),
+                        separatorBuilder: (_, index) {
+                          return const SizedBox(height: 10);
+                        },
+                        itemCount: selectedTasks.length))))
+      ],
+    );
+  }
+}
