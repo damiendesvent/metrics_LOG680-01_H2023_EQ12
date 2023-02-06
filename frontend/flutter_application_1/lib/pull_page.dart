717e4530c33f45282a478924d3eb2c3e6722d4e4
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-06 14:28:20 -0500

    Create pullPageLayout widget in pull_page.dart

---
+++ frontend/flutter_application_1/lib/pull_page.dart
@@ -0,0 +1,18 @@
+import 'main.dart';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PullPageLayout extends StatefulWidget {
+  const PullPageLayout({super.key});
+
+  @override
+  State<PullPageLayout> createState() => _PullPageLayout();
+}
+
+class _PullPageLayout extends State<PullPageLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
