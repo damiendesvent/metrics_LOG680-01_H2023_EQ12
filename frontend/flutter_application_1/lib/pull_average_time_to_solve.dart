71cc2d56d91f43a60161517c2ad54fbd2e3b9989
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-07 14:00:58 -0500

    Add pull_average_time_to_solve.dart Widget with UI

---
+++ frontend/flutter_application_1/lib/pull_average_time_to_solve.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PullAverageTimeToSolve extends StatefulWidget {
+  const PullAverageTimeToSolve({super.key});
+
+  @override
+  State<PullAverageTimeToSolve> createState() => _PullAverageTimeToSolveState();
+}
+
+class _PullAverageTimeToSolveState extends State<PullAverageTimeToSolve> {
+  String averageTime = "5h50";
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+      width: double.maxFinite,
+      padding: const EdgeInsets.all(15.0),
+      child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.spaceBetween,
+          children: [
+            Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 15,
+                children: [
+                  Icon(Icons.av_timer_rounded, size: 25),
+                  Text("Temps moyen de résolution d'une pull-request")
+                ]),
+            Chip(
+                avatar: const Icon(Icons.access_time_rounded),
+                backgroundColor: Colors.blue.shade100,
+                label: Text(
+                  "$averageTime", //"5d:5h",
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ))
+          ]),
+    ));
+  }
+}
