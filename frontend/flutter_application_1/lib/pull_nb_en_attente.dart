f7dbee7dbe5d82e5b605bc497e5cb86b62ae233e
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-07 12:59:40 -0500

    Create pull_nb_en_atttente.dart

---
+++ frontend/flutter_application_1/lib/pull_nb_en_attente.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PullNbEnAttente extends StatefulWidget {
+  const PullNbEnAttente({super.key});
+
+  @override
+  State<PullNbEnAttente> createState() => _PullNbEnAttenteState();
+}
+
+class _PullNbEnAttenteState extends State<PullNbEnAttente> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
