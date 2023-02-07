f37caadce52d3e3dae7d575ea91285f2ea280bba
damiendesvent <damiendesvent@gmail.com>
2023-02-07 15:21:00 -0500

    correction beug bad credential au demarrage

--- frontend/flutter_application_1/lib/main.dart
+++ frontend/flutter_application_1/lib/main.dart
@@ -49,43 +49,48 @@ class _MyHomePageState extends State<MyHomePage> {
       "Dashboard des métriques pour le laboratoire 1 de LOG680\n\nEquipe :\n - Damien\n - Bruno\n - Dorian Perthuis";
 
   int _pageDisplayIndex =
-  0; //'0' for the project tasks page and '1' for the pull requests page.
+      0; //'0' for the project tasks page and '1' for the pull requests page.
   final List<Widget> _bodyWidgets = [TasksPageLayout(), PullPageLayout()];
 
   late TextEditingController _projectController;
-  late TextEditingController _ownerController ;
+  late TextEditingController _ownerController;
   late TextEditingController _tokenController;
 
   Future<String> getProjectInfos() async {
     print("getProjectInfos called !");
 
+    //applique le projet par défaut si aucun projet n'est donné
+    if (_projectController.text.isEmpty &&
+        _ownerController.text.isEmpty &&
+        _tokenController.text.isEmpty) {
+      _projectController = TextEditingController(text: '3');
+      _ownerController = TextEditingController(text: 'damiendesvent');
+      _tokenController = TextEditingController(
+          text: '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');
+    }
+
     Uri graphQlUri = Uri.parse('https://api.github.com/graphql');
 
-    String query = "query{ user(login: \"${_ownerController.text}\") {  projectV2(number: ${_projectController.text}) { ... on ProjectV2 { items(first: 20) { nodes { fieldValues(last: 1) { nodes { ... on ProjectV2ItemFieldSingleSelectValue { name createdAt } } } content { ... on DraftIssue { title body } ... on Issue { __typename title state assignees(first: 10) { nodes { login } } } ... on PullRequest {__typename title state assignees(first: 10) { nodes { name } } } } creator { login } } } } } } } ";
+    String query =
+        "query{ user(login: \"${_ownerController.text}\") {  projectV2(number: ${_projectController.text}) { ... on ProjectV2 { items(first: 100) { nodes { fieldValues(last: 1) { nodes { ... on ProjectV2ItemFieldSingleSelectValue { name createdAt } } } content { ... on DraftIssue { title body } ... on Issue { __typename title state assignees(first: 10) { nodes { login } } } ... on PullRequest {__typename title state assignees(first: 10) { nodes { name } } } } creator { login } } } } } } } ";
 
     http.Response response = await http.post(graphQlUri,
         headers: {
-          'Authorization': 'Bearer ${_tokenController.text}', // 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
+          'Authorization': 'Bearer ${_tokenController.text}',
         },
         body: json.encode({
           "query": query,
         }));
 
-
-    /*String response_body =
-        '{ "data": { "node": { "items": { "nodes": [ { "fieldValues": { "nodes": [ { "name": "Done ✅", "createdAt": "2023-01-13T15:06:52Z" } ] }, "content": { "title": "Test issue", "assignees": { "nodes": [] } }, "creator": { "login": "elblogbruno" } }, { "fieldValues": { "nodes": [ { "name": "Done ✅", "createdAt": "2023-01-13T15:22:56Z" } ] }, "content": { "title": "test modèle issue", "assignees": { "nodes": [] } }, "creator": { "login": "damiendesvent" } }, { "fieldValues": { "nodes": [ { "name": "In progress 🛠️", "createdAt": "2023-01-18T15:00:36Z" } ] }, "content": { "title": "[FEATURE] Création de l\'UI de visualisation des métriques", "assignees": { "nodes": [ { "login": "Dorian-Perthuis" } ] } }, "creator": { "login": "damiendesvent" } }, { "fieldValues": { "nodes": [ { "name": "In progress 🛠️", "createdAt": "2023-01-23T00:19:11Z" } ] }, "content": { "title": "[FEATURE] Création des requêtes d\'import des métriques", "assignees": { "nodes": [ { "login": "damiendesvent" } ] } }, "creator": { "login": "damiendesvent" } }, { "fieldValues": { "nodes": [ { "name": "In progress 🛠️", "createdAt": "2023-01-18T15:02:49Z" } ] }, "content": { "title": "[FEATURE] Création de la base de données", "assignees": { "nodes": [ { "login": "elblogbruno" } ] } }, "creator": { "login": "damiendesvent" } }, { "fieldValues": { "nodes": [ { "name": "To do ⏲️", "createdAt": "2023-01-18T14:54:34Z" } ] }, "content": { "title": "[FEATURE] Création de l\'API Rest", "assignees": { "nodes": [] } }, "creator": { "login": "damiendesvent" } }, { "fieldValues": { "nodes": [ { "name": "Review 👀", "createdAt": "2023-01-23T00:25:59Z" } ] }, "content": { "title": "feat: started flutter project", "assignees": { "nodes": [] } }, "creator": { "login": "damiendesvent" } } ] } } } }';
-    */
     if (response.body.isNotEmpty) {
       var items = json.decode(response.body);
       print(items);
 
-      if (items['message'] != null)
-      {
-          return Future.error(items['message']);
+      if (items['message'] != null) {
+        return Future.error(items['message']);
       }
 
-      if (items['data']['user']['projectV2'] == null)
-      {
+      if (items['data']['user']['projectV2'] == null) {
         return Future.error("No project found");
       }
 
@@ -97,11 +102,10 @@ class _MyHomePageState extends State<MyHomePage> {
         int indexStatus = -1;
         for (var item in columns) {
           indexStatus =
-          item['name'] == status ? columns.indexOf(item) : indexStatus;
+              item['name'] == status ? columns.indexOf(item) : indexStatus;
         }
         if (indexStatus != -1) {
           columns[indexStatus]['number'] += 1;
-
         } else {
           columns.add({'name': status, 'number': 1});
         }
@@ -125,13 +129,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
     // restore the values from the shared preferences
     SharedPreferences.getInstance().then((prefs) {
-      _projectController.text = prefs.getInt('project_id').toString() ?? '';
+      _projectController.text = (prefs.getInt('project_id') ?? '').toString();
       _ownerController.text = prefs.getString('project_owner') ?? '';
-      _tokenController.text = prefs.getString('github_token') ?? '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';
+      _tokenController.text = prefs.getString('github_token') ?? '';
       //getProjectInfos();
     });
-
-
   }
 
   // @override
@@ -160,60 +162,60 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         drawer: _buildDrawer(),
         body: DefaultTextStyle(
-      style: Theme.of(context).textTheme.displayMedium!,
-      textAlign: TextAlign.center,
-      child: FutureBuilder<String>(
-        future: getProjectInfos(), // a previously-obtained Future<String> or null
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          List<Widget> children;
-          if (snapshot.hasData) {
-            return Container(
-                  padding: const EdgeInsets.all(20.0),
-                  child: tasks.isNotEmpty ? _bodyWidgets.elementAt(_pageDisplayIndex) : const Text('No project found'));
-
-          } else if (snapshot.hasError) {
-            children = <Widget>[
-              const Icon(
-                Icons.error_outline,
-                color: Colors.red,
-                size: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16),
-                child: Text('Error: ${snapshot.error}'),
-              ),
-            ];
-          } else {
-            children = const <Widget>[
-              SizedBox(
-                width: 60,
-                height: 60,
-                child: CircularProgressIndicator(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 16),
-                child: Text('Awaiting result...'),
-              ),
-            ];
-          }
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: children,
-            ),
-          );
-        },
-      ),
-    ));
+          style: Theme.of(context).textTheme.displayMedium!,
+          textAlign: TextAlign.center,
+          child: FutureBuilder<String>(
+            future:
+                getProjectInfos(), // a previously-obtained Future<String> or null
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              List<Widget> children;
+              if (snapshot.hasData) {
+                return Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: tasks.isNotEmpty
+                        ? _bodyWidgets.elementAt(_pageDisplayIndex)
+                        : const Text('No project found'));
+              } else if (snapshot.hasError) {
+                children = <Widget>[
+                  const Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
+                    size: 60,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Error: ${snapshot.error}'),
+                  ),
+                ];
+              } else {
+                children = const <Widget>[
+                  SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 16),
+                    child: Text('Awaiting result...'),
+                  ),
+                ];
+              }
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: children,
+                ),
+              );
+            },
+          ),
+        ));
   }
 
-
-  Widget _buildDrawer(){
+  Widget _buildDrawer() {
     return Drawer(
       child: ListView(
         children: <Widget>[
-          Container(
-              child: DrawerHeader(child: Text("$description"))),
+          Container(child: DrawerHeader(child: Text("$description"))),
           Container(
             child: Column(children: <Widget>[
               ListTile(
@@ -237,18 +239,16 @@ class _MyHomePageState extends State<MyHomePage> {
             ]),
           ),
           _buildProjectIdInput(),
-
         ],
       ),
     );
   }
 
-  Widget _buildProjectIdInput(){
+  Widget _buildProjectIdInput() {
     // add an input to enter project id and owner name
     // and a button to fetch the data from github
     return Container(
-      child:
-      Padding (
+      child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -276,14 +276,14 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton(
               onPressed: () async {
-                SharedApi.saveProjectInfo(int.parse(_projectController.text), _ownerController.text, _tokenController.text);
-                await Api().setProject(int.parse(_projectController.text), _ownerController.text, _tokenController.text);
+                SharedApi.saveProjectInfo(int.parse(_projectController.text),
+                    _ownerController.text, _tokenController.text);
+                await Api().setProject(int.parse(_projectController.text),
+                    _ownerController.text, _tokenController.text);
 
                 //getProjectInfos();
 
-                setState(() {
-
-                });
+                setState(() {});
               },
               child: const Text('Save'),
             ),
