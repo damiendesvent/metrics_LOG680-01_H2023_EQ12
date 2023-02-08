1c4c77c9b18fa6a4dc3267c1c357e576edbf4bab
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-08 11:48:56 -0500

    add pull_average_size.dart

---
+++ frontend/flutter_application_1/lib/pull_average_size.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PullAverageSize extends StatefulWidget {
+  const PullAverageSize({super.key});
+
+  @override
+  State<PullAverageSize> createState() => _PullAverageSizeState();
+}
+
+class _PullAverageSizeState extends State<PullAverageSize> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Container());
+  }
+}
