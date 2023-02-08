f3a13904f5b10c0af863a9a670d4fe7329b0eb89
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-08 10:14:14 -0500

    add metric's UI of PullOpenClose widget

--- frontend/flutter_application_1/lib/pull_rq_closed_open.dart
+++ frontend/flutter_application_1/lib/pull_rq_closed_open.dart
@@ -10,20 +10,60 @@ class PullOpenClose extends StatefulWidget {
 }
 
 class _PullOpenCloseState extends State<PullOpenClose> {
-  int nbPullRequestWaiting = 5;
+  double closePourcentage = 0.2;
+  var redTextStyle = TextStyle(
+      color: Color(0xffFF2F2F), fontWeight: FontWeight.w600, fontSize: 15);
+  var greenTextStyle = TextStyle(
+      color: Color(0xff00DB09), fontWeight: FontWeight.w600, fontSize: 15);
 
   @override
   Widget build(BuildContext context) {
     return Card(
         child: Container(
             padding: const EdgeInsets.all(15.0),
+            width: double.maxFinite,
             child: Wrap(
                 crossAxisAlignment: WrapCrossAlignment.center,
                 alignment: WrapAlignment.spaceBetween,
                 children: [
-                  Icon(Icons.pie_chart_outline_rounded),
-                  Text(
-                      "Rapport entre le nombre de pull-request ouverte et fermé")
+                  Wrap(
+                      spacing: 10,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Icon(Icons.pie_chart_outline_rounded),
+                        Text(
+                            "Rapport entre le nombre de pull-request fermé et ouverte"),
+                      ]),
+                  Wrap(
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    spacing: 5,
+                    children: [
+                      Text("${(closePourcentage) * 100}%", style: redTextStyle),
+                      DecoratedBox(
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(5.0)),
+                            gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.centerRight,
+                                colors: [
+                                  Color(0xffFF2F2F),
+                                  Color(0xffFF2F2F),
+                                  Color(0xff00DB09),
+                                  Color(0xff00DB09)
+                                ],
+                                stops: [
+                                  0,
+                                  closePourcentage,
+                                  closePourcentage,
+                                  1
+                                ]),
+                          ),
+                          child: SizedBox(height: 20, width: 80)),
+                      Text("${(1 - closePourcentage) * 100}%",
+                          style: greenTextStyle)
+                    ],
+                  )
                 ])));
   }
 }
