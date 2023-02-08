ee3bc445027dbd570c4dd5c54d5319669964dd2c
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-08 18:55:23 -0500

    add PullAverageComments widget

---
+++ frontend/flutter_application_1/lib/pull_nb_average_comments.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PullAverageComments extends StatefulWidget {
+  const PullAverageComments({super.key});
+
+  @override
+  State<PullAverageComments> createState() => _PullAverageCommentsState();
+}
+
+class _PullAverageCommentsState extends State<PullAverageComments> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Container());
+  }
+}
