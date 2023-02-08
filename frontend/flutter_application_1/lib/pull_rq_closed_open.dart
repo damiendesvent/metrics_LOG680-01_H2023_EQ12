2d46a40586c2f9da780c5a35c75740eab9a872fd
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-02-08 09:04:59 -0500

    Add pull_rq_close_open.dart widget

---
+++ frontend/flutter_application_1/lib/pull_rq_closed_open.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class PullOpenClose extends StatefulWidget {
+  const PullOpenClose({super.key});
+
+  @override
+  State<PullOpenClose> createState() => _PullOpenCloseState();
+}
+
+class _PullOpenCloseState extends State<PullOpenClose> {
+  int nbPullRequestWaiting = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
