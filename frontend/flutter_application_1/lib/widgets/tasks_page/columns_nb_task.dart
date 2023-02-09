6a6d10602f8ff6cdd45f365c2fab3fb4de6d1deb
damiendesvent <damiendesvent@gmail.com>
2023-02-09 17:01:47 -0500

    ajout d'un switch (front)

--- frontend/flutter_application_1/lib/widgets/tasks_page/columns_nb_task.dart
+++ frontend/flutter_application_1/lib/widgets/tasks_page/columns_nb_task.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
 import '../../main.dart';
+
+bool showPullRequests = true;
 
 class ColumnsNbTask extends StatefulWidget {
   const ColumnsNbTask({super.key});
@@ -15,16 +16,34 @@ class _ColumnsNbTask extends State<ColumnsNbTask> {
     return Column(children: [
       Card(
           child: Container(
-            width: double.maxFinite,
-            padding: const EdgeInsets.all(15.0),
-            child: Wrap(
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 15,
-                children: const [
-                  Icon(Icons.calculate_rounded, size: 25),
-                  Text("Tâches actives par colonne")
-                ]),
-          )),
+        width: double.maxFinite,
+        padding: const EdgeInsets.all(15.0),
+        child: Row(children: [
+          Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              spacing: 15,
+              children: const [
+                Icon(Icons.calculate_rounded, size: 25),
+                Text("Tâches actives par colonne")
+              ]),
+          Spacer(),
+          Container(
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(width: 0.5)),
+              padding: EdgeInsets.all(5),
+              child: Row(children: [
+                Switch(
+                    value: showPullRequests,
+                    onChanged: (value) => setState(() {
+                          showPullRequests = value;
+                        })),
+                Text(' voir les Pull Request ',
+                    style: TextStyle(fontWeight: FontWeight.w600))
+              ]))
+        ]),
+      )),
       const SizedBox(height: 3.0),
       Row(
         children: columns.map((Map item) {
@@ -32,31 +51,27 @@ class _ColumnsNbTask extends State<ColumnsNbTask> {
               child: Card(
                   child: Center(
                       child: Container(
-                          height: 100,
-                          padding: const EdgeInsets.all(15.0),
-                          child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: [
-                                  Text("${item['name']}"),
-                                  Container(
-                                      height: 30,
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 3, horizontal: 20),
-                                      decoration: BoxDecoration(
-                                          border: Border.all(
-                                              color: Colors.black54, width: 1.5),
-                                          borderRadius: const BorderRadius.all(
-                                              Radius.circular(20))),
-                                      child: Text("${item['number']}",
-                                          style: const TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.w700))),
-                                ],
-                              ),
-
-
-                          ))));
+            height: 100,
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text("${item['name']}"),
+                Container(
+                    height: 30,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black54, width: 1.5),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20))),
+                    child: Text("${item['number']}",
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w700))),
+              ],
+            ),
+          ))));
         }).toList(),
       )
     ]);
