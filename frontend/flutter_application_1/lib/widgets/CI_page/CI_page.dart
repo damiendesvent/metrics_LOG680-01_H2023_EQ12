4447bad820a18689dfed4827c8b19e27120c015d
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-03-10 10:12:58 -0500

    Create Ci_page.dart

---
+++ frontend/flutter_application_1/lib/widgets/CI_page/CI_page.dart
@@ -0,0 +1,18 @@
+import '../../main.dart';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CIPageLayout extends StatefulWidget {
+  const CIPageLayout({super.key});
+
+  @override
+  State<CIPageLayout> createState() => _CIPageLayoutState();
+}
+
+class _CIPageLayoutState extends State<CIPageLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
