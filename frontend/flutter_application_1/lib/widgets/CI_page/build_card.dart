2ff2b42e3d8087827e7913e1de4af31e8d96c6db
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-03-10 10:13:14 -0500

    Create build_card.dart

---
+++ frontend/flutter_application_1/lib/widgets/CI_page/build_card.dart
@@ -0,0 +1,12 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class BuildCard extends StatelessWidget {
+  BuildCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("build"));
+  }
+}
