3ef2ce70255db0ac194e02844ca37acf665094b3
Dorian-Perthuis <78471044+Dorian-Perthuis@users.noreply.github.com>
2023-03-13 18:07:09 -0400

    Create build_nb widget with front

---
+++ frontend/flutter_application_1/lib/widgets/CI_page/build_nb.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+int nbBuild = 5;
+
+class BuildNb extends StatefulWidget {
+  const BuildNb({super.key});
+
+  @override
+  State<BuildNb> createState() => _BuildNbState();
+}
+
+class _BuildNbState extends State<BuildNb> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Container(
+      padding: const EdgeInsets.all(15.0),
+      child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          alignment: WrapAlignment.spaceBetween,
+          children: [
+            Wrap(
+                crossAxisAlignment: WrapCrossAlignment.center,
+                spacing: 15,
+                children: [
+                  Icon(Icons.numbers_rounded, size: 25),
+                  Text("Nombre de Build")
+                ]),
+            Container(
+                height: 30,
+                padding:
+                    const EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black54, width: 1.5),
+                    borderRadius: const BorderRadius.all(Radius.circular(20))),
+                child: Text("$nbBuild",
+                    style: const TextStyle(
+                        fontSize: 20, fontWeight: FontWeight.w700)))
+          ]),
+    ));
+  }
+}
