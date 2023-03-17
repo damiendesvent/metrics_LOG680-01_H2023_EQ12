787b62f89bb7776ae98c2c0fa2495000b6c59ab1
Bruno Moya <elblogdebruno@outlook.es>
2023-03-16 21:48:59 -0400

    refacture of code

--- frontend/flutter_application_1/lib/widgets/tasks_page/task_list.dart
+++ frontend/flutter_application_1/lib/widgets/tasks_page/task_list.dart
@@ -162,8 +162,8 @@ class _TaskListState extends State<TaskList> {
   }
 
   String getAverageLeadTime(List tasks) {
-    if (tasks.isEmpty || !isCustomCard) {
-      return "0";
+    if (tasks == null || tasks.isEmpty || !isCustomCard) {
+      return "Aucune donnée";
     }
 
     double sum = 0;
@@ -272,53 +272,37 @@ class _TaskListState extends State<TaskList> {
           //       ),
           //     )),
 
-          if (selectedTasks.isNotEmpty && getAverageLeadTime(selectedTasks) != "0")
-
-            Card(
-                child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    child:  Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Tooltip(
-                              message: "Lead time moyen",
-                              child:Chip(
-                                  avatar: const Icon(Icons.access_time_rounded),
-                                  backgroundColor: Colors.blue.shade100,
-                                  label: Text(
-                                    getAverageLeadTime(selectedTasks), //"5d:5h",
-                                    style: TextStyle(fontWeight: FontWeight.w600),
-                                  ))),
-                          const SizedBox(width: 10),
-                          Tooltip(
-                              message: "Nombre de tâches",
-                              child: Chip(
-                                  avatar: const Icon(Icons.list),
-                                  backgroundColor: Colors.blue.shade100,
-                                  label: Text(
-                                    getNumberOfTasks(selectedTasks), //"5d:5h",
-                                    style: TextStyle(fontWeight: FontWeight.w600),
-                                  ))),
-                        ]
-                    )
-                ))
-
-
-          else
-            Card(
-                child: Container(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Tooltip(
-                        message: "Lead time moyen",
-                        child: Chip(
-                            avatar: const Icon(Icons.access_time_rounded),
-                            backgroundColor: Colors.blue.shade100,
-                            label: Text(
-                              "Aucune Donnée", //"5d:5h",
-                              style: TextStyle(fontWeight: FontWeight.w600),
-                            )))))
+          Card(
+              child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  child:  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Tooltip(
+                            message: "Lead time moyen",
+                            child:Chip(
+                                avatar: const Icon(Icons.access_time_rounded),
+                                backgroundColor: Colors.blue.shade100,
+                                label: Text(
+                                  getAverageLeadTime(selectedTasks), //"5d:5h",
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ))),
+                        const SizedBox(width: 10),
+                        Tooltip(
+                            message: "Nombre de tâches",
+                            child: Chip(
+                                avatar: const Icon(Icons.list),
+                                backgroundColor: Colors.blue.shade100,
+                                label: Text(
+                                  getNumberOfTasks(selectedTasks), //"5d:5h",
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ))),
+                      ]
+                  )
+              ))
         ]),
+
         const SizedBox(height: 3),
         if (selectedTasks.isEmpty)
           const Expanded(child: Center(child: Text("Aucune tâche")))
